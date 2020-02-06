17b12056d506c318e0d4e637a46f1e587dad225b
javieragui <javieraguilararribas@yahoo.es>
2020-02-06 18:09:58 +0100

    Commit ListView2DDB

---
+++ src/main/java/com/example/listviewdb2/ViewHolder.java
@@ -0,0 +1,11 @@
+package com.example.listviewdb2;
+
+import android.view.View;
+import android.widget.RatingBar;
+
+public class ViewHolder {
+    RatingBar rate = null;
+    ViewHolder(View base){
+        this.rate=(RatingBar)base.findViewById(R.id.rate);
+    }
+}
